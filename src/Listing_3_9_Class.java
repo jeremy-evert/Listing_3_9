e8946d240eb9fc2a3b3e4f6d0fcf039c6b5c8b38
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-18 17:58:48 -0500

    Added conditional expressions

--- src/Listing_3_9_Class.java
+++ src/Listing_3_9_Class.java
@@ -12,56 +12,42 @@ public class Listing_3_9_Class {
 		System.out.print("Enter the day of the month you were born: ");
 		int day = input.nextInt();
 		
-		
 		String chineseZodiacYear = null;
+		String zodiacSign = null;
 		
 		switch (year % 12){
-		case 0:  chineseZodiacYear = "monkey."; 	break; 
-		case 1:  chineseZodiacYear = "rooster."; 	break;
-		case 2:  chineseZodiacYear = "dog."; 		break;
-		case 3:  chineseZodiacYear = "pig."; 		break;
-		case 4:  chineseZodiacYear = "rat."; 		break;
-		case 5:  chineseZodiacYear = "ox."; 		break;
-		case 6:  chineseZodiacYear = "tiger."; 		break;
-		case 7:  chineseZodiacYear = "rabbit."; 	break;
-		case 8:  chineseZodiacYear = "dragon."; 	break;
-		case 9:  chineseZodiacYear = "snake."; 		break;
-		case 10: chineseZodiacYear = "horse."; 		break;
-		case 11: chineseZodiacYear = "sheep."; 
+		case 0:  chineseZodiacYear = "monkey"; 	break; 
+		case 1:  chineseZodiacYear = "rooster"; 	break;
+		case 2:  chineseZodiacYear = "dog"; 		break;
+		case 3:  chineseZodiacYear = "pig"; 		break;
+		case 4:  chineseZodiacYear = "rat"; 		break;
+		case 5:  chineseZodiacYear = "ox"; 		break;
+		case 6:  chineseZodiacYear = "tiger"; 		break;
+		case 7:  chineseZodiacYear = "rabbit"; 	break;
+		case 8:  chineseZodiacYear = "dragon"; 	break;
+		case 9:  chineseZodiacYear = "snake"; 		break;
+		case 10: chineseZodiacYear = "horse"; 		break;
+		case 11: chineseZodiacYear = "sheep"; 
 		}
-		
-		System.out.println("\nYou were born in the year of the " + chineseZodiacYear);
-		
-		System.out.print("You are a ");
-		
-		switch (month){
-		case 1:	if (day<20) {System.out.println("Capricorn."); 	break;}
-				else 		{System.out.println("Aquarius."); 	break;}
-		case 2:	if (day<19)	{System.out.println("Aquarius."); 	break;}
-				else		{	System.out.println("Pices."); 	break;}
-		case 3:	if (day<21)	{System.out.println("Pices."); 		break;}
-				else		{System.out.println("Aries."); 		break;}
-		case 4:	if (day<20)	{System.out.println("Aries."); 		break;}
-				else		{System.out.println("Taurus."); 	break;}
-		case 5:	if (day<21)	{System.out.println("Taurus."); 	break;}
-				else		{System.out.println("Gemini."); 	break;}
-		case 6:	if (day<22)	{System.out.println("Gemini."); 	break;}
-				else		{System.out.println("Cancer."); 	break;}
-		case 7:	if (day<23)	{System.out.println("Cancer."); 	break;}
-				else		{System.out.println("Leo."); 		break;}
-		case 8:	if (day<24)	{System.out.println("Leo."); 		break;}
-				else		{System.out.println("Virgo."); 		break;}
-		case 9:	if (day<23)	{System.out.println("Virgo."); 		break;}
-				else		{System.out.println("Libra."); 		break;}
-		case 10: if (day<23){System.out.println("Libra."); 		break;}
-				else		{System.out.println("Scorpio."); 	break;}
-		case 11: if (day<22){System.out.println("Scorpio."); 	break;}
-				else		{System.out.println("Sagittarius.");break;}
-		case 12: if (day<22){System.out.println("Sagittarius.");	  }
-				else		{System.out.println("Capricorn."); 		  }
 
+		switch (month){
+		case 1:	 zodiacSign = (day<20) ? "Capricorn."	: "Aquarius."; 		break;
+		case 2:  zodiacSign = (day<19) ? "Aquarius."  	: "Pices."; 		break;
+		case 3:	 zodiacSign = (day<21) ? "Pices."     	: "Aries."; 		break;
+		case 4:	 zodiacSign = (day<20) ? "Aries."     	: "Taurus."; 		break;
+		case 5:	 zodiacSign = (day<21) ? "Taurus."    	: "Gemini."; 		break;
+		case 6:	 zodiacSign = (day<22) ? "Gemini." 		: "Cancer."; 		break;
+		case 7:	 zodiacSign = (day<23) ? "Cancer." 		: "Leo."; 			break;
+		case 8:	 zodiacSign = (day<24) ? "Leo." 		: "Virgo."; 		break;
+		case 9:	 zodiacSign = (day<23) ? "Virgo." 		: "Libra."; 		break;
+		case 10: zodiacSign = (day<23) ? "Libra." 		: "Scorpio."; 		break;
+		case 11: zodiacSign = (day<22) ? "Scorpio." 	: "Sagittarius.";	break;
+		case 12: zodiacSign = (day<22) ? "Sagittarius." : "Capricorn."; 		  
 		}
+		
+		
+		System.out.print("\nYou were born in the year of the " + chineseZodiacYear);
+		System.out.print(" and you are a " + zodiacSign);
 
 	}
-
 }
