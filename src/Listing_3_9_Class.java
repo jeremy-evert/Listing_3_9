8590e4ae7c02c6f9511caf5fd5cd32fe1d4f267a
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-18 17:30:09 -0500

    Cleaned up my code

--- src/Listing_3_9_Class.java
+++ src/Listing_3_9_Class.java
@@ -12,7 +12,7 @@ public class Listing_3_9_Class {
 		System.out.print("Enter the day of the month you were born: ");
 		int day = input.nextInt();
 		
-		System.out.print("You were born in the year of the ");
+		System.out.print("\nYou were born in the year of the ");
 		
 		switch (year % 12){
 		case 0: System.out.println("monkey."); break;
@@ -32,78 +32,30 @@ public class Listing_3_9_Class {
 		System.out.print("You are a ");
 		
 		switch (month){
-		case 1:
-			if (day<20){
-				System.out.println("Capricorn."); break;
-			}else{
-				System.out.println("Aquarius."); break;
-			}
-		case 2:
-			if (day<19){
-				System.out.println("Aquarius."); break;
-			}else{
-				System.out.println("Pices."); break;
-			}
-		case 3:
-			if (day<21){
-				System.out.println("Pices."); break;
-			}else{
-				System.out.println("Aries."); break;
-			}
-		case 4:
-			if (day<20){
-				System.out.println("Aries."); break;
-			}else{
-				System.out.println("Taurus."); break;
-			}
-		case 5:
-			if (day<21){
-				System.out.println("Taurus."); break;
-			}else{
-				System.out.println("Gemini."); break;
-			}
-		case 6:
-			if (day<22){
-				System.out.println("Gemini."); break;
-			}else{
-				System.out.println("Cancer."); break;
-			}
-		case 7:
-			if (day<23){
-				System.out.println("Cancer."); break;
-			}else{
-				System.out.println("Leo."); break;
-			}
-		case 8:
-			if (day<24){
-				System.out.println("Leo."); break;
-			}else{
-				System.out.println("Virgo."); break;
-			}
-		case 9:
-			if (day<23){
-				System.out.println("Virgo."); break;
-			}else{
-				System.out.println("Libra."); break;
-			}
-		case 10:
-			if (day<23){
-				System.out.println("Libra."); break;
-			}else{
-				System.out.println("Scorpio."); break;
-			}
-		case 11:
-			if (day<22){
-				System.out.println("Scorpio."); break;
-			}else{
-				System.out.println("Sagittarius."); break;
-			}
-		case 12:
-			if (day<22){
-				System.out.println("Sagittarius."); break;
-			}else{
-				System.out.println("Capricorn."); break;
-			}
+		case 1:	if (day<20) {System.out.println("Capricorn."); 	break;}
+				else 		{System.out.println("Aquarius."); 	break;}
+		case 2:	if (day<19)	{System.out.println("Aquarius."); 	break;}
+				else		{	System.out.println("Pices."); 	break;}
+		case 3:	if (day<21)	{System.out.println("Pices."); 		break;}
+				else		{System.out.println("Aries."); 		break;}
+		case 4:	if (day<20)	{System.out.println("Aries."); 		break;}
+				else		{System.out.println("Taurus."); 	break;}
+		case 5:	if (day<21)	{System.out.println("Taurus."); 	break;}
+				else		{System.out.println("Gemini."); 	break;}
+		case 6:	if (day<22)	{System.out.println("Gemini."); 	break;}
+				else		{System.out.println("Cancer."); 	break;}
+		case 7:	if (day<23)	{System.out.println("Cancer."); 	break;}
+				else		{System.out.println("Leo."); 		break;}
+		case 8:	if (day<24)	{System.out.println("Leo."); 		break;}
+				else		{System.out.println("Virgo."); 		break;}
+		case 9:	if (day<23)	{System.out.println("Virgo."); 		break;}
+				else		{System.out.println("Libra."); 		break;}
+		case 10: if (day<23){System.out.println("Libra."); 		break;}
+				else		{System.out.println("Scorpio."); 	break;}
+		case 11: if (day<22){System.out.println("Scorpio."); 	break;}
+				else		{System.out.println("Sagittarius.");break;}
+		case 12: if (day<22){System.out.println("Sagittarius.");	  }
+				else		{System.out.println("Capricorn."); 		  }
 
 		}
 
